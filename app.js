2d66f6e9c91ea8be9d922bc52e80203e866a072e
jessedegroothva <jesse.de.groot2@hva.nl>
2023-03-28 17:58:55 +0200

    update changes

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ const fs = require("fs");
 const key = fs.readFileSync("localhost-key.pem", "utf-8");
 const cert = fs.readFileSync("localhost.pem", "utf-8");
 
-
+// responds to the webpage with the express library
 const express = require('express')
+
 // applicatie wordt gemaakt
+// de nodige infrastructuur opzet om een webapplicatie te kunnen bouwen en verwerken.
 const app = express();
 
-// probeer css in te laden
-app.use(express.static(__dirname + '/'));
+// library wordt gebruikt om de Middleware & static files te gebruiken
+app.use(express.static('public'));
 
-
+// library wordt gebruikt om een template engine te gebruiken voor html pagina's
 app.set('view engine', 'ejs');
+
+// logger wordt gebruikt om de informatie te loggen in de terminal
 app.use(logger)
 
 // fetch de data van de API op index pagina
@@ -23,9 +27,22 @@ app.get('/', (req, res) => {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      // console.log(data)
       const artObjects = data.artObjects
       res.render('index', { data: { artObjects } }); // pass the data object to the template
+    })
+    .catch(error => console.error(error));
+});
+
+
+// fetch de data van de API op overview pagina
+app.get('/overview', (req, res) => {
+  const url = 'https://www.rijksmuseum.nl/api/nl/collection?key=9ZKSEiYs&involvedMaker=Rembrandt+van+Rijn';
+  
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const artObjects = data.artObjects.slice(0, 10); // slice the first 10 art objects
+      res.render('overview', { data: { artObjects } }); // pass the data object to the template
     })
     .catch(error => console.error(error));
 });
@@ -38,26 +55,12 @@ app.get('/details/:objectNumber', (req, res) => {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      // console.log(data)
       const artObject = data.artObject;
       res.render('details', { artObject });
     })
     .catch(error => console.error(error));
 });
 
-// fetch de data van de API op overview pagina
-app.get('/overview', (req, res) => {
-  const url = 'https://www.rijksmuseum.nl/api/nl/collection?key=9ZKSEiYs&involvedMaker=Rembrandt+van+Rijn';
-  
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      // console.log(data)
-      const artObjects = data.artObjects.slice(0, 10); // slice the first 10 art objects
-      res.render('overview', { data: { artObjects } }); // pass the data object to the template
-    })
-    .catch(error => console.error(error));
-});
 
 // get search data
 app.get('/search', (req, res) => {
@@ -65,13 +68,10 @@ app.get('/search', (req, res) => {
       res.render('search'); // pass the data object to the template
     });
 
-// Middleware & static files
-app.use(express.static('public'));
-
 // userRouter is een mini router
-const routes = require('./routes');
+// const routes = require('./routes');
 
-app.use('/', routes);
+// app.use('/', routes);
 
 function logger(req, res, next) {
     // console.log('req.originalUrl', req.originalUrl)
